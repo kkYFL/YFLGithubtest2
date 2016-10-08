16c6f0a791e7fde35ac1dc05fba4778433b13e10
杨丰林 <1490719714@qq.com>
2016-10-08 16:57:04 +0800

    创建测试项目

---
+++ YFLGithubtest2/YFLGithubtest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YFLGithubtest2
+//
+//  Created by 杨丰林 on 16/10/8.
+//  Copyright © 2016年 杨丰林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
